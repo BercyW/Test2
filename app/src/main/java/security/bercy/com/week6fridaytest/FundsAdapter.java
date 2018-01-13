4fcf4a4939ed7013ca73db84980ccffd61717839
Bercy <adnmks@gmail.com>
2018-01-13 17:36:13 -0500

    11

--- app/src/main/java/security/bercy/com/week6fridaytest/FundsAdapter.java
+++ app/src/main/java/security/bercy/com/week6fridaytest/FundsAdapter.java
@@ -1,10 +1,12 @@
 package security.bercy.com.week6fridaytest;
 
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -23,13 +25,14 @@ public class FundsAdapter extends RecyclerView.Adapter<FundsAdapter.ViewHolder> 
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
-
-        private final TextView tv_id;
-        private final TextView tv_investmentName;
-        private final TextView tv_agency;
+        View fundView;
+        TextView tv_id;
+        TextView tv_investmentName;
+        TextView tv_agency;
 
         public ViewHolder(View itemView) {
             super(itemView);
+            fundView = itemView;
             tv_id = itemView.findViewById(R.id.tv_id);
             tv_investmentName = itemView.findViewById(R.id.tv_investmentName);
             tv_agency = itemView.findViewById(R.id.tv_agency);
@@ -39,7 +42,15 @@ public class FundsAdapter extends RecyclerView.Adapter<FundsAdapter.ViewHolder> 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_funds,parent,false);
-        ViewHolder viewHolder = new ViewHolder(view);
+        final ViewHolder viewHolder = new ViewHolder(view);
+        viewHolder.fundView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int position = viewHolder.getAdapterPosition();
+                funds funds = fundsList.get(position);
+
+            }
+        });
 
         return viewHolder;
     }
@@ -47,7 +58,7 @@ public class FundsAdapter extends RecyclerView.Adapter<FundsAdapter.ViewHolder> 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
         funds funds = fundsList.get(position);
-        holder.tv_id.setText(funds.getId());
+        holder.tv_id.setText(funds.getId()+"");
         holder.tv_investmentName.setText(funds.getInvestmentName());
         holder.tv_agency.setText(funds.getAgency());
     }
